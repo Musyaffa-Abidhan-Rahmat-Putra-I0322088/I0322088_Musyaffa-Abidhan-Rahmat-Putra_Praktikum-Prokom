eb4db8c6253f5a3484699666a9ab6a82073c161b
Musyaffa-Abidhan-Rahmat-Putra-I0322088 <127175827+Musyaffa-Abidhan-Rahmat-Putra-I0322088@users.noreply.github.com>
2023-03-14 12:03:02 +0700

    MENGUMPULKAN TUGAS 1

---
+++ Exercise-01.py
@@ -0,0 +1,9 @@
+#TUGAS PRAPROKOM 1
+print("==========================================")
+print("TUGAS PRAKTIKUM PROGRAMA KOMPUTER 1")
+print("==========================================")
+print("NAMA : Musyaffa Abidhan Rahmat Putra")
+print("NIM  : I0322088")
+print()
+print("             Halo semua perkenalkan saya Abid. Saya Berasal dari Gunungkidul, Yogyakarta. Gunungkidul terkenal akan keindahan pantainya.Rumah saya cukup dekat dengan pantai hanya kurang lebih 30 menit perjalanan.")
+print("             Harapan saya sebagai lulusan Teknik Industri adalah saya berharap mampu bekerja di Industri-industri besar. saya juga berharap mampu mengembangkan skill saya terutama skil yang berkaitan dengan industri.")
