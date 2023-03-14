d3562cb1534a0edfe03128cc3297a29196c17bb6
Musyaffa-Abidhan-Rahmat-Putra-I0322088 <127175827+Musyaffa-Abidhan-Rahmat-Putra-I0322088@users.noreply.github.com>
2023-03-14 11:57:12 +0700

    MENGUMPULKAN TUGAS 1 PRAKTIKUM

--- IDENTITAS.py
+++ IDENTITAS.py
@@ -1,6 +1,7 @@
+print("TUGAS PRATIKUM PROGRAMA KOMPUTER")
 nama_depan= "Musyaffa"
 nama_belakang= "Putra"
 
 print (nama_depan)
 print ("Namaku rahmat")
-print ("namaku"+ nama_depan )
+print ("Namaku"+ nama_depan )
